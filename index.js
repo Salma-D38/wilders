c5f489b3e68bd2df8862cd3648d06966b1a1b24b
Salma DESMAZIERES <s.desmazieres@yahoo.fr>
2024-03-19 10:43:46 +0100

    atelier terminé

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const wilder1 = "Kana";
+const wilder2 = "Bastien";
+const wilder3 = "Leo-paul";
+const wilder4 = "Salma";
+
+console.log(chalk.blue(wilder1));
+console.log(chalk.green(wilder2));
+console.log(chalk.red(wilder3));
+console.log(chalk.yellow(wilder4));
